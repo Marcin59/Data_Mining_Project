76609013edf13df9e59a2d5ed265375df289be8c
marcin <marcin.kapiszewski@gmail.com>
2024-04-07 00:47:17 +0200

    Create initial Preprocessing

--- utilities.py
+++ utilities.py
@@ -2,9 +2,11 @@ import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
 
-def getNumericalColumns(df: pd.DataFrame):
-    return df.select_dtypes(include=[np.int64, np.float64])
-
-def fillNans(df: pd.DataFrame):
-    imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-    return pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
+def load_data():
+    train = pd.read_csv("./data/train.csv")
+    test = pd.read_csv("./data/test.csv").merge(pd.read_csv("./data/sample_submission.csv"), how="inner")
+    y_train = train["SalePrice"]
+    X_train = train.drop(columns = ["SalePrice"])
+    y_test = test["SalePrice"]
+    X_test = test.drop(columns = ["SalePrice"])
+    return X_train, y_train, X_test, y_test

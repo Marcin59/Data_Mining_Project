ed4a048771a53d4da9d0ed517182fa281734d05f
marcin <marcin.kapiszewski@gmail.com>
2024-04-19 00:09:21 +0200

    Prepare initial pipeline for preproccesed data and unpreproccesed

--- utilities.py
+++ utilities.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.base import BaseEstimator, TransformerMixin
 
 def load_data():
     train = pd.read_csv("./data/train.csv")
@@ -10,3 +13,60 @@ def load_data():
     y_test = test["SalePrice"]
     X_test = test.drop(columns = ["SalePrice"])
     return X_train, y_train, X_test, y_test
+
+def remove_null_columns(df: pd.DataFrame, threshold = 0.4):
+    num_of_nulls = df.isna().sum()
+    percentage_of_nulls = num_of_nulls/df.shape[0]
+    null_columns = percentage_of_nulls.loc[percentage_of_nulls > threshold].index
+    return df.drop(null_columns, axis = 1), null_columns
+
+def remove_low_variance_columns(df: pd.DataFrame, threshold = 0.02):
+    sel = VarianceThreshold(threshold=threshold)
+    sel.fit(df)
+    removed_columns = df.columns[np.bitwise_not(sel.get_support())]
+    return pd.DataFrame(sel.transform(df), columns = sel.get_feature_names_out()), removed_columns
+
+def remove_high_correlation_columns(df: pd.DataFrame, threshold = 0.9):
+    correlations = df.corr().iloc
+    column_indexes_to_remove = set()
+    for row_index, row in enumerate(correlations):
+        if row_index in column_indexes_to_remove: continue
+        for col_index, elem in enumerate(row[row_index+1:], start=row_index+1):
+            if abs(elem) >  threshold:
+                column_indexes_to_remove.add(col_index)
+    columns_to_remove = df.columns[list(column_indexes_to_remove)]
+    return df.drop(columns=columns_to_remove), columns_to_remove
+
+def remove_low_correlation_to_target_columns(df: pd.DataFrame, target: np.ndarray, threshold = 0.05):
+    df["target"] = target
+    target_col_corr = df.corr()["target"]
+    df = df.drop(["target"], axis=1)
+    column_indexes_to_remove = set()
+    for index, value in enumerate(target_col_corr.iloc):
+        if abs(value) < threshold:
+            column_indexes_to_remove.add(index)
+    columns_to_remove = df.columns[list(column_indexes_to_remove)]
+    return df.drop(columns=columns_to_remove), columns_to_remove
+
+class ColumnDropperTransformer():
+    def __init__(self, selected_columns):
+        self.selected_columns = selected_columns
+
+    def transform(self,X,y=None):
+        return X[self.selected_columns]
+
+    def fit(self, X: pd.DataFrame, y=None):
+        return self 
+
+class CategoricalEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+    
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        X_copy = X.copy()
+        for col in X_copy.columns:
+            X_copy[col] = LabelEncoder().fit_transform(X_copy[col])
+        return X_copy
